aab663eeacdebe0ab8d820ed4cc80f5a49dac767
Erick Lopez <erick83@gmail.com>
2021-02-21 16:20:38 -0400

    Fixed layout change and map mark position

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,12 @@
-import * as React from 'react';
-import {SafeAreaView, View, StyleSheet, Button} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, View, StyleSheet, Button, Dimensions} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+
 import DrawerComponent from '../components/drawer.component';
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    // ...StyleSheet.absoluteFillObject,
-    height: '85%',
-    // width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
@@ -19,20 +17,34 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    height: '15%',
     paddingLeft: 10,
     paddingRight: 10,
   },
-});
+};
 
 const HomeScreen = ({navigation}) => {
   const [loc, setloc] = React.useState();
-  Geolocation.getCurrentPosition((info) => {
-    setloc({
-      latitude: info.coords.latitude,
-      longitude: info.coords.longitude,
+  const [screen, setscreen] = React.useState(Dimensions.get('window'));
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((info) => {
+      setloc({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+      });
     });
-  });
+  }, []);
+
+  function checkOrientation(portrait, landscape) {
+    if (screen.width > screen.height) {
+      return landscape;
+    }
+    return portrait;
+  }
+
+  function layoutChange() {
+    setscreen(Dimensions.get('window'));
+  }
 
   function buttonHandler() {
     navigation.navigate('Users');
@@ -41,14 +53,34 @@ const HomeScreen = ({navigation}) => {
   return (
     <DrawerComponent navigation={navigation}>
       <SafeAreaView>
-        <View style={styles.container}>
-          <MapView
-            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-            style={styles.map}>
-            {loc && <Marker coordinate={loc} />}
-          </MapView>
+        <View
+          style={{
+            ...styles.container,
+            height: checkOrientation(
+              Dimensions.get('screen').height - 140,
+              '75%',
+            ),
+          }}
+          onLayout={layoutChange}>
+          {loc && (
+            <MapView
+              region={{
+                latitude: loc.latitude,
+                longitude: loc.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+              provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+              style={styles.map}>
+              <Marker coordinate={loc} />
+            </MapView>
+          )}
         </View>
-        <View style={styles.buttonContainer}>
+        <View
+          style={{
+            ...styles.buttonContainer,
+            height: checkOrientation(60, '25%'),
+          }}>
           <Button onPress={buttonHandler} color="#2d2c3c" title="Users List" />
         </View>
       </SafeAreaView>
