73525ffbe88709a59a01b61b892e8f1b05b25df6
Erick Lopez <erick83@gmail.com>
2021-02-21 19:39:18 -0400

    hb button animate

--- src/components/hb-button.component.js
+++ src/components/hb-button.component.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {TouchableHighlight} from 'react-native';
-import {useDispatch} from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {TouchableHighlight, Animation} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import {View} from 'react-native';
 
 import {actionCreators} from '../redux/controls/actions';
@@ -22,18 +22,38 @@ const styles = {
 
 const HbButtonComponent = () => {
   const dispatch = useDispatch();
+  const [close, setclose] = useState(null);
+  const show = useSelector((store) => store.controls.drawerShow);
 
   function hbButtonClick() {
     dispatch(actionCreators.toggleDrawer());
   }
 
+  useEffect(() => {
+    if (show) {
+      setclose({
+        top: {
+          transform: [{rotate: '-45deg'}, {translateX: -2}, {translateY: 2}],
+        },
+        bottom: {
+          transform: [{rotate: '45deg'}, {translateX: -2}, {translateY: -2}],
+        },
+        mid: {
+          display: 'none',
+        },
+      });
+    } else {
+      setclose(null);
+    }
+  }, [show]);
+
   return (
     <View style={styles.bgContainer}>
       <TouchableHighlight onPress={hbButtonClick}>
         <View style={styles.linesContainer}>
-          <View style={styles.hbLine} />
-          <View style={styles.hbLine} />
-          <View style={styles.hbLine} />
+          <View style={[styles.hbLine, close ? close.top : null]} />
+          <View style={[styles.hbLine, close ? close.mid : null]} />
+          <View style={[styles.hbLine, close ? close.bottom : null]} />
         </View>
       </TouchableHighlight>
     </View>
